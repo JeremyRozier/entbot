917e271166e21ed671694b8639b72f391a233947
JeremyRozier <jeremy.rozier70@gmail.com>
2024-06-17 23:28:30 +0200

    Fixed bug in check_module

--- user_functions.py
+++ user_functions.py
@@ -10,8 +10,8 @@ def check_module(module_name: str):
     Ensures necessary dependencies are downloaded
     to make the bots work.
     """
-    if module_name in sys.modules:
-        q = input("aiohttp est manquant, veux tu l'installer ? [y/n] : ")
+    if module_name not in sys.modules:
+        q = input(f"{module_name} est manquant, veux tu l'installer ? [y/n] : ")
         if q.lower() == "y":
             subprocess.check_call(
                 [sys.executable, "-m", "pip", "install", module_name]

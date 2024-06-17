fb489e6613aa9e6c07f529342c0961d7d7c661db
JeremyRozier <jeremy.rozier70@gmail.com>
2024-06-17 23:31:31 +0200

    Fixed a bug in check_module by using import_module

--- user_functions.py
+++ user_functions.py
@@ -3,6 +3,7 @@ and modify them if necessary to make the bots work."""
 
 import subprocess
 import sys
+from importlib import import_module
 
 
 def check_module(module_name: str):
@@ -10,8 +11,12 @@ def check_module(module_name: str):
     Ensures necessary dependencies are downloaded
     to make the bots work.
     """
-    if module_name not in sys.modules:
-        q = input(f"{module_name} est manquant, veux tu l'installer ? [y/n] : ")
+    try:
+        import_module(module_name)
+    except ModuleNotFoundError:
+        q = input(
+            f"{module_name} est manquant, veux tu l'installer ? [y/n] : "
+        )
         if q.lower() == "y":
             subprocess.check_call(
                 [sys.executable, "-m", "pip", "install", module_name]
